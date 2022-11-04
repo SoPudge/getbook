b2e61bf3e6d91987230cb7f8466346b3bc98d4ee
SoPudge <20067852+SoPudge@users.noreply.github.com>
2022-11-04 22:25:27 +0800

    feat: 新增epub下载生成和打包功能

--- getbook.py
+++ getbook.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
+import shutil
 import pdb
+import datetime
 import codecs
 import os
 import re
@@ -25,24 +27,21 @@ class Pack(object):
     def txt_handler(self, file):
         """Description
                 传入未经清洗的txt文件，输出清洗过后的txt文件，按行返回，只做基本的清洗。
-                每次调用输出一个清洗后的行，生成html工作由txt_to_html完成
+                每次调用输出一个章节数组，数组第一个元素是章节名称，后面每个元素是章节每段
 
-        @param file:  传入需要清洗的txt文件目录，类似file='./lib/xxx/title-author.txt'
-        @type  file:  String
+        @parm
+            file: string, 传入需要清洗的txt文件目录，类似file='./lib/xxx/title-author.txt':
 
-        @return: 返回一个行即可
-        @rtype : str, line
+        @return: 
+            list: 数组第一个元素是章节名称，后面是章节每个段落
 
         @raise e:  None
         """
-        # logger = Logger('txt_handler')
+        logger = Logger('txt_handler')
 
         # 章节正则表达式
         re_char_chn = re.compile(
             r'^\s*[第卷][0123456789一二三四五六七八九十零〇百千两]*[章?回?部?节?集?卷?].*', re.S)
-        # re_char_eng = re.compile(
-            # r'^\s*[c-zC-z]{7,7}\s*[0-9]*.*[\n|\r|\r\n]', re.S)
-        #re_title = re.compile(r'《(.*)》.*作者：(.*).txt', re.S)
 
         encodings = {'UTF-16': 'utf16', 'ISO-8859-1': 'gbk',
                      'UTF-8-SIG': 'utf8', 'ascii': 'gbk', 'GB2312': 'gbk'}
@@ -52,22 +51,23 @@ class Pack(object):
             l = f.read(500)
             encoding = chardet.detect(l)['encoding']
 
-        # 正则表达式按line获取文章正文
-
-        #存储章节标题数组
+        #title_url只存储章节名称，不清空直到存储满
         title_url = []
+        #title_content存储章节名称+章节分段内容，用于每次yield
         title_content = []
+
         with codecs.open(file, 'rb', encodings[encoding], 'ignore') as f:
             while True:
                 line = f.readline()
-                #存储title+正文一个章节的数组
-                #测试章节标题正则表达式
-                title_item = re_char_chn.findall(line.strip('\r\n').strip('\u3000'))
-                # 结束吻端读取
+                # 存储title+正文一个章节的数组
+                # 测试章节标题正则表达式
+                title_item = re_char_chn.findall(
+                    line.strip('\r\n').strip('\u3000'))
+                # 结束文段读取
                 if not line:
                     break
-                # 遇到章节存储，返回上一个title_content，然后下次先清空title_content再存储
-                # 每次返回时确定title_content是否为空，如为空一般是首次返回跳过
+                #如果章节标题不为空，且长度小于30则直接append到相应数组中
+                #如果章节名称的数组titlle_url为空，则证明是第一章节之前的内容跳过，否则说明读取到n+1个章节标题，此时yield第n个章节的title_content
                 elif title_item != [] and len(title_item[0]) < 30:
                     if title_url == []:
                         pass
@@ -86,7 +86,7 @@ class Pack(object):
                     continue
                 else:
                     title_content.append(line.strip('\r\n').strip('\u3000'))
-            #到最后一章节时候yield倒数第二章内容，最后一章单独yield出来
+            # 到最后一章节时候yield倒数第二章内容，最后一章单独yield出来
             yield title_content
 
     def txt_to_html(self, book_details, book_type='mobi'):
@@ -96,123 +96,200 @@ class Pack(object):
             读取txt文件和其中的目录，生成相关的text.html文件，如果是已经生成过text.html文件的抓取类型
             则跳过上述生成步骤，直接返回传入的title_details
         Args: 
-            book_details: 类型同抓取方法类当中的返回一致，即[title,author]
+            book_details: 类型同抓取方法类当中的返回一致，即[title,author,bookid]
+            book_type: mobi|epub，进行不同打包形式
         Returns: 
-            title_url: 生成text.html在temp目录当中，同时返回title_url的list，格式[0,章节名称]
+            title_url: 生成text.html在temp目录当中，同时返回title_url的list，格式[章节名称]
         '''
 
-
-        html_page_start = '<?xml version="1.0" encoding="utf-8" ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN"><head><metahttp-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8"/><meta name="generator" content="https://github.com/SoPudge" /><title>^Chapter^</title><link rel="stylesheet" href="style.css" type="text/css" /></head><body>\n'
+        html_page_start = '<?xml version="1.0" encoding="utf-8" ?>\n<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">\n<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN">\n<head>\n<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8"/>\n<meta name="generator" content="https://github.com/SoPudge" />\n<title>^Chapter^</title>\n<link rel="stylesheet" href="style.css" type="text/css" />\n</head>\n<body>\n'
         html_page_end = '</body></html>'
+
+        # toc_start = '<?xml version="1.0" encoding="utf-8" standalone="no"?> <!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN" "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd"><ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1"><head><meta name="cover" content="cover"/><meta name="dtb:uid" content="epub-^date^" /><meta name="dtb:depth" content="1"/><meta name="dtb:generator" content="Created By SoPudge - https://github.com/SoPudge"/><meta name="dtb:totalPageCount" content="0"/><meta name="dtb:maxPageNumber" content="0"/></head><docTitle><text>^docTitle^</text></docTitle><docAuthor><text>^docAuthor^</text></docAuthor>\n'
+        toc_end = '</navMap></ncx>'
+
+        opf_end = '</spine>\n<guide>\n<reference href="cover.html" type="cover" title="Cover"/>\n<reference href="book-toc.html" type="toc" title="Table Of Contents"/>\n<reference href="chapter1.html" type="text" title="Beginning"/>\n</guide>\n</package>\n'
+
+        cover_content = '<?xml version="1.0" encoding="utf-8" ?>\n<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-CN">\n<head>\n<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />\n<meta name="generator" content="EasyPub v1.50" />\n<title> Cover </title>\n<style type="text/css">\n.cover {height:100%; text-align:center;padding:0px;}\n</style>\n<link rel="stylesheet" href="style.css" type="text/css"/>\n</head>\n<body>\n<div class="cover">\n<img src="cover.jpg" alt="^title^" height="720" class="cover" />\n</div>\n</body>\n</html>\n'
 
         title = book_details[0]
         author = book_details[1]
+        bookid = book_details[2]
 
         logger.info('开始制作书籍：%s' % title)
 
-        #epub制作开始
-        # 分章节写入html文件到OEBPS文件夹，格式chapter1.html，chpater2.html
+        # epub制作开始
+
+        # 写入mimetype文件
+        shutil.copy('temp/epub/mimetype', 'lib/%s/mimetype' % title)
+
+        # 写入meta-inf文件
+        shutil.copy('temp/epub/container.xml',
+                    'lib/%s/META-INF/container.xml' % title)
+
+        # 写入cover.html封面
+        with open('lib/%s/OEBPS/cover.html' % title, 'w') as f:
+            f.write(cover_content.replace('^title^', title))
+
+        # 写入复制css文件
+        shutil.copy('temp/epub/style.css', 'lib/%s/OEBPS/style.css' % title)
+
+        # 移动封面位置
+        shutil.move('lib/%s/cover.jpg' % title, 'lib/%s/OEBPS/' % title)
+
+        # 写入book-toc前缀文件，内容通过for循环插入
+        shutil.copy('temp/epub/book-toc.html',
+                    'lib/%s/OEBPS/book-toc.html' % title)
+
+        # 1.写入toc.ncx文件当中需要插入的内容，目录通过for循环插入
+        shutil.copy('temp/epub/toc.ncx', 'lib/%s/OEBPS/toc.ncx' % title)
+
+        with open('lib/%s/OEBPS/toc.ncx' % title, 'r') as f:
+            s = f.read()
+        with open('lib/%s/OEBPS/toc.ncx' % title, 'w+') as f:
+            f.write(s.replace('^title^', title).replace(
+                '^author^', author).replace('^bookid^', bookid))
+
+        # 3. 分章节写入html文件到OEBPS文件夹，格式chapter1.html，chpater2.html，包括目录文件等
         n = 1
         for chapter in self.txt_handler('lib/%s/%s-%s.txt' % (title, title, author)):
-            print(chapter[0])
+            # print(chapter[0])
             with open('lib/%s/OEBPS/chapter%s.html' % (title, n), 'w') as f:
-                #替换html声明当中标题并重新写入
-                f.write(html_page_start.replace('^Chapter^',chapter[0]))
-                #写入body后第一个章节标题，写入toc.ncx，写入opf文件
-                #向html当中写入标题
-                f.write('<h2 id="title" class="titlel2std">%s</h2>\n' % chapter[0])
-                #写入toc.ncx文件
-                #todo
-                #写入opf文件
-                #todo
-                #按行写入正文，跳过第一个章节标题
+                # 替换html声明当中标题并重新写入
+                f.write(html_page_start.replace('^Chapter^', chapter[0]))
+                # 写入body后第一个章节标题，写入toc.ncx，写入opf文件
+                # 向html当中写入标题
+                f.write('<h2 id="title" class="titlel2std">%s</h2>\n' %
+                        chapter[0])
+                # 按行写入正文，跳过第一个章节标题
                 for content in chapter[1:]:
                     f.write('<p class="a">%s</p>\n' % content)
                 f.write(html_page_end)
+            # 写入toc.ncx的目录，除去封面和目录，第一章从n=3插入
+            with open('lib/%s/OEBPS/toc.ncx' % title, 'a') as f:
+                f.write('<navPoint id="chapter%s" playOrder="%s"><navLabel><text>%s</text></navLabel><content src="chapter%s.html"/></navPoint>\n' %
+                        (n, n+2, chapter[0], n))
+            # 写入book-toc文件
+            with open('lib/%s/OEBPS/book-toc.html' % title, 'a') as f:
+                f.write(
+                    '<dt class="tocl2"><a href="chapter%s.html">%s</a></dt>\n' % (n, chapter[0]))
             n = n + 1
+        # 写入一些文档toc的end文件
+        with open('lib/%s/OEBPS/toc.ncx' % title, 'a') as f:
+            f.write(toc_end)
 
- 
+        # 写入bool-toc的end文件
+        with open('lib/%s/OEBPS/book-toc.html' % title, 'a') as f:
+            f.write('</dl>\n</div>\n</body>\n</html>')
+
+        # 一次性写入opf文件
+        shutil.copy('temp/epub/content.opf',
+                    'lib/%s/OEBPS/content.opf' % title)
+
+        with open('lib/%s/OEBPS/content.opf' % title, 'r') as f:
+            s = f.read()
+
+        with open('lib/%s/OEBPS/content.opf' % title, 'w+') as f:
+            f.write(s.replace('^bookid^', bookid).replace('^title^', title).replace(
+                '^author^', author).replace('^year^', str(datetime.datetime.now().year)))
+
+        with open('lib/%s/OEBPS/content.opf' % title, 'a') as f:
+            for i in range(1, n):
+                f.write(
+                    '<item id="chapter%s" href="chapter%s.html" media-type="application/xhtml+xml"/>\n' % (i, i))
+
+            f.write('</manifest>\n<spine toc="ncxtoc">\n<itemref idref="cover" linear="no"/>\n<itemref idref="htmltoc" linear="yes"/>\n')
+
+            for i in range(1, n):
+                f.write('<itemref idref="chapter%s" linear="yes"/>\n' % i)
+            f.write(opf_end)
+
     def res_to_book(self, url, book_type='mobi'):
         logger = Logger('res_to_mobi')
         '''
         Description: 
             打包ncx,opf,text.html,style.css文件为mobi文件，先读取临时文件，再写入实际内容保存
         Args:
-            title_url，章节名称的dict，格式是{0:titlename,1:titlename}，从0开始
+            url: 小说url用于传入到下载方法当中调用
         Returns:
-            N/A, 生成mobi文件保存在目录当中
+            N/A, 生成mobi|epub文件保存在目录当中
         '''
 
-        # 生成html文件，同时创建文件目录，执行txt_to_html，并返回目录结构数组
-        [title, author] = self.d.zxcs(url)
-
-        # 根据book_type创建不同子目录
-        # mobi无需创建，直接在根目录使用
-        # epub创建下列目录
         if book_type == 'epub':
+            logger.info('开始创建epub文件')
+        # 生成html文件，同时创建文件目录，执行txt_to_html，并返回目录结构数组
+            [title, author, bookid] = self.d.zxcs(url)
+            # 创建目录epub
             os.mkdir('lib/%s/OEBPS' % title)
             os.mkdir('lib/%s/META-INF' % title)
+            # 执行txt文件转换html方法，根据book_type不同创建不同的子文件
+            title_url = self.txt_to_html([title, author, bookid])
 
-        # 执行txt文件转换html方法，根据book_type不同创建不同的子文件
-        title_url = self.txt_to_html([title, author])
+            # 清除txt文件并开始打包epub为zip文件
+            os.remove('lib/%s/%s-%s.txt' % (title, title, author))
 
-        # 读取模板ncx文件，获取其中内容
-        with open('temp/mobi/toc.ncx', 'rt') as f:
-            ncx = f.read()
-        ncxstart = ncx.split('$toclist')[0]
-        ncxend = ncx.split('$toclist')[1]
+            logger.info('开始打包epub文件')
+            # 打包zip文件
+            os.system('cd lib/%s/ && zip %s-%s.epub -0 -X mimetype && zip %s-%s.epub -9 -r META-INF OEBPS' % (title,title,author,title,author))
 
-        # 读取模板opf文件获取其中内容
-        with open('temp/mobi/title.opf', 'rt') as f:
-            opf = f.read()
+        if book_type == 'mobi':
+            # 读取模板ncx文件，获取其中内容
+            with open('temp/mobi/toc.ncx', 'rt') as f:
+                ncx = f.read()
+            ncxstart = ncx.split('$toclist')[0]
+            ncxend = ncx.split('$toclist')[1]
 
-        # 读取css文件
-        with open('temp/mobi/style.css', 'rt') as f:
-            stylecss = f.read()
+            # 读取模板opf文件获取其中内容
+            with open('temp/mobi/title.opf', 'rt') as f:
+                opf = f.read()
 
-        # 读取cover文件
-        with open('temp/cover.jpg', 'rb') as f:
-            cover = f.read()
+            # 读取css文件
+            with open('temp/mobi/style.css', 'rt') as f:
+                stylecss = f.read()
 
-        # 先写入ncx文件
-        with open('lib/%s/toc.ncx' % title, 'at') as f:
-            f.write(ncxstart)
-            if len(title_url) != 0:
-                for n in range(len(title_url)):
-                    f.write('<navPoint id="navpoint-%s" playOrder="%s"><navLabel><text>%s</text></navLabel><content src="text.html#id%s"/></navPoint>' %
-                            (n+1, n+1, title_url[n][0], n+1))
-                    f.write('\n')
-            else:
-                f.write(
-                    '<navPoint id="navpoint" playOrder="1"><navLabel><text>%s</text></navLabel><content src="text.html"/></navPoint>' % '全部')
-                f.write(ncxend)
-        logger.info('NCX制作完毕')
+            # 读取cover文件
+            with open('temp/cover.jpg', 'rb') as f:
+                cover = f.read()
 
-        # 写入OPF文件
-        with open('lib/%s/%s-%s.opf' % (title, title, author), 'at') as f:
-            opf = opf.replace('$title', title).replace('$author', author)
-            f.write(opf)
-        logger.info('OPF制作完毕')
+            # 先写入ncx文件
+            with open('lib/%s/toc.ncx' % title, 'at') as f:
+                f.write(ncxstart)
+                if len(title_url) != 0:
+                    for n in range(len(title_url)):
+                        f.write('<navPoint id="navpoint-%s" playOrder="%s"><navLabel><text>%s</text></navLabel><content src="text.html#id%s"/></navPoint>' %
+                                (n+1, n+1, title_url[n][0], n+1))
+                        f.write('\n')
+                else:
+                    f.write(
+                        '<navPoint id="navpoint" playOrder="1"><navLabel><text>%s</text></navLabel><content src="text.html"/></navPoint>' % '全部')
+                    f.write(ncxend)
+            logger.info('NCX制作完毕')
 
-        # 写入css文件
-        with open('lib/%s/style.css' % title, 'at') as f:
-            f.write(stylecss)
-        logger.info('style.css制作完毕')
+            # 写入OPF文件
+            with open('lib/%s/%s-%s.opf' % (title, title, author), 'at') as f:
+                opf = opf.replace('$title', title).replace('$author', author)
+                f.write(opf)
+            logger.info('OPF制作完毕')
 
-        # 写入cover文件
-        with open('lib/%s/cover.jpg' % title, 'ab') as f:
-            f.write(cover)
-        logger.info('封面制作完毕')
+            # 写入css文件
+            with open('lib/%s/style.css' % title, 'at') as f:
+                f.write(stylecss)
+            logger.info('style.css制作完毕')
 
-        # 开始转换mobi
-        logger.info('开始转换mobi')
-        workdir = os.getcwd() + '/lib/%s/' % title
-        workopf = '%s/%s-%s.opf' % (workdir, title, author)
-        workkindlegen = os.getcwd() + '/kindlegen/kindlegen'
-        out = os.popen('%s -c1 -dont_append_source -locale zh %s' %
-                       (workkindlegen, workopf)).read()
-        # print(out)
-        logger.info(out)
+            # 写入cover文件
+            with open('lib/%s/cover.jpg' % title, 'ab') as f:
+                f.write(cover)
+            logger.info('封面制作完毕')
+
+            # 开始转换mobi
+            logger.info('开始转换mobi')
+            workdir = os.getcwd() + '/lib/%s/' % title
+            workopf = '%s/%s-%s.opf' % (workdir, title, author)
+            workkindlegen = os.getcwd() + '/kindlegen/kindlegen'
+            out = os.popen('%s -c1 -dont_append_source -locale zh %s' %
+                           (workkindlegen, workopf)).read()
+            # print(out)
+            logger.info(out)
 
 
 class Download(object):
@@ -384,13 +461,27 @@ class Download(object):
         title = re_titleauthor.match(titleauthor).group(1)
         author = re_titleauthor.match(titleauthor).group(2)
 
-        # 确认是存在书籍目录
+        # 获取小说封面链接
+        d = r.get('http://zxcs.me/post/%s' % bookurl.split('/')[-1])
+        c = d.text
+        tree = html.fromstring(c)
+        #多种不同的cover获取url，针对zxcs
+        if tree.xpath('/html/body/div[4]/div[2]/div[2]/img/@src'):
+            cover = tree.xpath('/html/body/div[4]/div[2]/div[2]/img/@src')[0]
+        else:
+            cover = tree.xpath('/html/body/div[4]/div[2]/div[2]/a/img/@src')[0]
+        # cover = tree.xpath('/html/body/div[4]/div[2]/div[2]/a/img/@src')[0]
+
+        # 确认是存在书籍目录，如果存在直接删除重新创建
         if title in os.listdir('lib'):
             logger.info('书籍目录已存在！清除已有内容，重新生成文件！')
-            [os.remove('lib/%s/%s' % (title, file))
-             for file in os.listdir('lib/%s' % title)]
-        else:
-            os.mkdir('lib/%s' % title)
+            shutil.rmtree('lib/%s' % title)
+        os.mkdir('lib/%s' % title)
+
+        # 下载封面
+        c = r.get(cover)
+        with open('lib/%s/cover.jpg' % title, 'wb') as f:
+            f.write(c.content)
 
         # 直接下载文件
         l = r.get(dl_url[0])
@@ -414,7 +505,7 @@ class Download(object):
                 os.rename("lib/%s/%s" % (title, file),
                           "lib/%s/%s-%s.txt" % (title, title, author))
         logger.info('清理目录删除txt外文件并重命名txt')
-        return [title, author]
+        return [title, author, bookurl.split('/')[-1]]
 
     def jjxs(self, bookurl):
         logger = Logger('jjxs')
@@ -473,11 +564,11 @@ class Download(object):
 if __name__ == '__main__':
 
     p = Pack()
-    # p.res_to_book('http://www.zxcs.me/post/13396')
-    file = './lib/我的成神日志/我的成神日志-大力宝.txt'
-    os.makedirs('lib/我的成神日志/OEBPS/', exist_ok=True)
-    p.txt_to_html(['我的成神日志', '大力宝'])
+    p.res_to_book('http://www.zxcs.me/post/9721', 'epub')
+    #file = './lib/我的成神日志/我的成神日志-大力宝.txt'
+    #os.makedirs('lib/我的成神日志/OEBPS/', exist_ok=True)
+    #p.txt_to_html(['我的成神日志', '大力宝'])
 
     # p.txt_handler(file)
     # for i in p.txt_handler(file):
-        # pass
+    # pass
